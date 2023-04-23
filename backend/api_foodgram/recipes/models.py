b77308be80aa6532d33fa4a39618c5edbbcaf6b4
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-04-23 21:55:48 +0300

    model done

--- backend/api_foodgram/recipes/models.py
+++ backend/api_foodgram/recipes/models.py
@@ -6,13 +6,13 @@ User = get_user_model()
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200, verbose_name='name')
-    color = models.CharField(max_length=7)
-    slug = models.SlugField(unique=True, max_length=200, verbose_name='slug')
+    name = models.CharField(max_length=200, verbose_name='Name', unique=True)
+    color = models.CharField(max_length=7, verbose_name='Color', unique=True)
+    slug = models.SlugField(max_length=200, verbose_name='Slug', unique=True)
 
     class Meta:
-        verbose_name = 'tag'
-        verbose_name_plural = 'tags'
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
         ordering = ['name']
 
     def __str__(self):
@@ -20,16 +20,12 @@ class Tag(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200, verbose_name='name')
-    measurement_unit = models.CharField(max_length=200, verbose_name='unit')
-    amount = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(1)],
-        verbose_name='amount'
-    )
+    name = models.CharField(max_length=200, verbose_name='Name')
+    measurement_unit = models.CharField(max_length=200, verbose_name='Unit')
 
     class Meta:
-        verbose_name = 'ingredient'
-        verbose_name_plural = 'ingredients'
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
         ordering = ['name']
 
     def __str__(self):
@@ -37,38 +33,41 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=200, verbose_name='name')
-    text = models.TextField(verbose_name='text')
+    name = models.CharField(max_length=200, verbose_name='Name', unique=True)
+    text = models.TextField(verbose_name='Text')
     cooking_time = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(1)],
-        verbose_name='time',
+        validators=[
+            MinValueValidator(1, message='specify a time of at least 1 minute')
+        ],
+        verbose_name='Cooking time'
     )
-    image = models.BinaryField(verbose_name='image')
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        verbose_name='Image'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='recipes',
-        verbose_name='author',
+        verbose_name='Author'
     )
     tag = models.ManyToManyField(
         Tag,
         through='RecipeTag',
         related_name='recipes',
-        verbose_name='tag',
+        verbose_name='Tag'
     )
     ingredient = models.ManyToManyField(
         Ingredient,
         through='RecipeIngredient',
         related_name='recipes',
-        verbose_name='ingredient',
+        verbose_name='Ingredient'
     )
-    pub_date = models.DateTimeField(
-        verbose_name='pub_date', auto_now_add=True)
 
     class Meta:
-        verbose_name = 'recipe'
-        verbose_name_plural = 'recipes'
-        ordering = ['-pub_date']
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+        ordering = ['-id']
         constraints = [
             models.UniqueConstraint(
                 fields=('name', 'author'),
@@ -81,8 +80,16 @@ class Recipe(models.Model):
 
 
 class RecipeTag(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Recipe'
+    )
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+        verbose_name='Tag'
+    )
 
     class Meta:
         constraints = [
@@ -97,8 +104,24 @@ class RecipeTag(models.Model):
 
 
 class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='amount',
+        verbose_name='Recipe'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='amount',
+        verbose_name='Ingredient'
+    )
+    amount = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1, 'specify the weight of at least 1 unit')
+        ],
+        verbose_name='Amount'
+    )
 
     class Meta:
         constraints = [
@@ -110,3 +133,55 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f'{self.recipe_id} {self.ingredient_id}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorite',
+        verbose_name='User'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorite',
+        verbose_name='Recipe'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique_favorite_recipe'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} {self.recipe}'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='cart',
+        verbose_name='User'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='cart',
+        verbose_name='Recipe'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique_cart_recipe'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} {self.recipe}'
--- backend/api_foodgram/users/models.py
+++ backend/api_foodgram/users/models.py
@@ -6,34 +6,66 @@ class User(AbstractUser):
 
     class Roles(models.TextChoices):
 
-        USER = 'user'
         ADMIN = 'admin'
+        USER = 'user'
 
     email = models.EmailField(
         max_length=254,
         unique=True,
-        verbose_name='email',
+        verbose_name='Email'
     )
     first_name = models.CharField(
         max_length=150,
-        verbose_name='first_name',
+        verbose_name='First_name'
     )
     last_name = models.CharField(
         max_length=150,
-        verbose_name='last_name',
+        verbose_name='Last_name'
     )
     role = models.CharField(
         max_length=9,
         choices=Roles.choices,
         default=Roles.USER,
-        verbose_name='role',
+        verbose_name='Role'
     )
 
     class Meta:
-        verbose_name = 'user'
-        verbose_name_plural = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
         ordering = ['username']
 
     @property
     def is_admin(self):
         return self.role == self.Roles.ADMIN or self.is_superuser
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Follower'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Author'
+    )
+
+    class Meta:
+        verbose_name = 'Follow'
+        verbose_name_plural = 'Follows'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_follow'
+            ),
+            models.CheckConstraint(
+                check=models.Q(user=models.F('user')),
+                name='self_subscribe'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} {self.author}'
