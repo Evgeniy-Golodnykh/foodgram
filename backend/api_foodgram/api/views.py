4186bdf9d81ca1e010b74cd05f04b32bc3d9cfa4
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-05-02 23:42:00 +0300

    add download cart func

--- backend/api_foodgram/api/views.py
+++ backend/api_foodgram/api/views.py
@@ -1,5 +1,10 @@
-from rest_framework import filters, mixins, permissions, viewsets
+from datetime import datetime
+
+from django.db import models
+from django.http import HttpResponse
+from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .permissions import IsAdminOrAuthorOrReadOnly
 from .serializers import (
@@ -7,7 +12,9 @@ from .serializers import (
     IngredientSerializer, RecipeSerializer, TagSerializer,
 )
 from .tools import create_destroy_instances
-from recipes.models import Cart, Favorite, Ingredient, Recipe, Tag
+from recipes.models import (
+    Cart, Favorite, Ingredient, Recipe, RecipeIngredient, Tag,
+)
 
 
 class CreateDestroyListViewSet(mixins.CreateModelMixin,
@@ -59,3 +66,27 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return create_destroy_instances(
             Recipe, Cart, FavoriteOrCartRecipeSerializer, request, pk
         )
+
+    @action(detail=False, methods=['get'])
+    def download_shopping_cart(self, request):
+        if not request.user.cart.exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        ingredients = RecipeIngredient.objects.filter(
+            recipe__cart__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(amount=models.Sum('amount'))
+        cart = (
+            f'{request.user.get_full_name()} shopping list includes:\n'
+        )
+        cart += '\n'.join([
+            f'- {ingredient["ingredient__name"]} / '
+            f'{ingredient["amount"]} '
+            f'{ingredient["ingredient__measurement_unit"]}'
+            for ingredient in ingredients
+        ])
+        filename = f'Shopping_cart_{datetime.today():%Y-%m-%d %H:%M}.txt'
+        response = HttpResponse(cart, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename={filename}'
+        return response
