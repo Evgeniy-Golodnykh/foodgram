38e671bd4498c7c52dec3ca1b87d4909be5db28c
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-05-14 18:51:43 +0300

    done

--- backend/api_foodgram/api/serializers.py
+++ backend/api_foodgram/api/serializers.py
@@ -3,9 +3,7 @@ from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
-from recipes.models import (
-    Cart, Favorite, Ingredient, Recipe, RecipeIngredient, Tag,
-)
+from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
 from users.serializers import CustomUserSerializer
 
 User = get_user_model()
