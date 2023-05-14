0521df997ae2aa6e9c9861fb910fa0f9f0cfd978
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-05-14 20:18:37 +0300

    done

--- backend/api_foodgram/users/serializers.py
+++ backend/api_foodgram/users/serializers.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
-from .models import Follow
 from recipes.models import Recipe
 
 User = get_user_model()
@@ -26,7 +25,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request', False)
-        return request and obj.follower.filter(user=request.user).exists()
+        return request and obj.follow.filter(user=request.user).exists()
 
 
 class CustomUserCreateSerializer(serializers.ModelSerializer):
@@ -93,7 +92,7 @@ class FollowSerializer(serializers.ModelSerializer):
         )
 
     def get_is_subscribed(self, obj):
-        return Follow.objects.filter(author=obj.author, user=obj.user).exists()
+        return obj.follow.filter(user=self.request.user).exists()
 
     def get_recipes(self, obj):
         queryset = Recipe.objects.filter(author=obj.author)
